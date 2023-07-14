86f2a68016fa3fa3109405fb82eaca96595213b4
amykad <amina.kaddal.1@gmail.com>
2023-07-14 07:52:17 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,33 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
 /**
- * main - Determines ifa number is positive, negative or zero.
+ * main - prints if number is positive, zero or negative
+ * Return: Always (Success)
  *
- * Return: Always 0.
  */
+
 int main (void)
 {
-	int n:
-	srand(time(0)):
-	n = rand() - RAND_MAX / 2:
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-		printf(%d is positive\n". n):
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf(%d is zero\n". n):
+	printf("%d is zero\n", n):
 	}
 	else
 	{
-		printf(%d is negative\n" . n):
+	printf("%d is negative\n", n):
 	}
-	return (0):
+	return (0);
 } 
