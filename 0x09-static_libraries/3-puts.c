845ad37048946ff57b5ec5d03ec4f5151a41823e
root <amina.kaddal.1@gmail.com>
2023-10-29 17:07:40 -0700

    apdate

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
-void _puts(char *str)
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -9,5 +9,5 @@ void _puts(char *str)
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+return (0);
 }
