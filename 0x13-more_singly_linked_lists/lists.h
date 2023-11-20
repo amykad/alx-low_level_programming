2309788267cc05b8153d9a91d17f12a3ef68885f
amykad <amina.kaddal@gmail.com>
2023-11-20 12:39:11 -0800

    apdate all

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,8 +16,7 @@ typedef struct listint_s
 {
 int n;
 struct listint_s *next;
-}
-listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -36,3 +35,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
